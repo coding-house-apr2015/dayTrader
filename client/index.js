cfc1a408ff12c56ec63bb4c44bc2da6df3068e51
Chyld Medford <chyld.medford@gmail.com>
2015-04-22 17:22:59 -0700

    fin

--- client/index.js
+++ client/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-angular.module('day-trader', ['firebase']);
+angular.module('day-trader', ['firebase'])
+.run(['$rootScope', '$window', function($rootScope, $window){
+  $rootScope.fbRoot = new $window.Firebase('https://day-trader-x.firebaseio.com/');
+}])
+.controller('master', ['$scope', '$firebaseObject', '$firebaseArray', '$http', function($scope, $firebaseObject, $firebaseArray, $http){
+  $http.jsonp('http://dev.markitondemand.com/Api/v2/Quote/jsonp?symbol=AAPL&callback=JSON_CALLBACK').then(function(response){
+    console.info(response);
+  });
+}]);
